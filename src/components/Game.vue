4d3cce3eba01c6e18ad9a11aa7f1138abadb65fe
Lennert Claeys <lennert.claeys@teamleader.eu>
2022-04-04 17:50:42 +0200

    Render the surface

---
+++ src/components/Game.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { onMounted, ref } from "vue";
+import generate from "../data/terrain/generator";
+import Surface from "../data/terrain/surface";
+import Renderer from "../renderers/emojiRenderer/renderer";
+
+// defineProps<{}>();
+
+const canvas = ref<HTMLDivElement | null>(null);
+
+onMounted(() => {
+  const surface = new Surface(96, 54, generate);
+
+  const renderer = new Renderer();
+  renderer.render(canvas.value as HTMLDivElement, surface);
+});
+</script>
+
+<template>
+  <div ref="canvas"></div>
+</template>
+
+<style scoped></style>
