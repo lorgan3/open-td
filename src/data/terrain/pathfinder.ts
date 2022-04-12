5e4da82c4f9b7d4483ca8a9d35853213db366cd9
Lennert Claeys <lennert.claeys@teamleader.eu>
2022-04-12 17:51:01 +0200

    Change pathfinder to consider diagonal tiles
    Paths would just go between 2 tiles otherwise

--- src/data/terrain/pathfinder.ts
+++ src/data/terrain/pathfinder.ts
@@ -3,14 +3,14 @@ import Tile, { TileType } from "./tile";
 import Heap from "heap";
 
 const NEIGHBORS = [
-  [-1, -1],
-  [1, -1],
-  [-1, 1],
-  [1, 1],
   [0, -1],
   [-1, 0],
   [1, 0],
   [0, 1],
+  [-1, -1],
+  [1, -1],
+  [-1, 1],
+  [1, 1],
 ];
 
 export const DEFAULT_COSTS: Partial<Record<TileType, number>> = {
@@ -66,23 +66,46 @@ class Pathfinder {
       }
 
       const currentHash = current.getHash();
-      NEIGHBORS.forEach(([x, y], index) => {
-        const neighbor = this.surface.getTile(
-          x + current.getX(),
-          y + current.getY()
-        );
 
+      const neighbors = NEIGHBORS.map(([x, y]) =>
+        this.surface.getTile(x + current.getX(), y + current.getY())
+      );
+
+      // Unrolled for performance I guess
+      const costs: Array<number | null | undefined> = [];
+      costs[0] = neighbors[0] ? costFn(neighbors[0]) : null;
+      costs[1] = neighbors[1] ? costFn(neighbors[1]) : null;
+      costs[2] = neighbors[2] ? costFn(neighbors[2]) : null;
+      costs[3] = neighbors[3] ? costFn(neighbors[3]) : null;
+      costs[4] =
+        neighbors[4] && costs[0] && costs[1]
+          ? this.getCost(costFn, neighbors[4], costs[0] + costs[1])
+          : null;
+      costs[5] =
+        neighbors[5] && costs[0] && costs[2]
+          ? this.getCost(costFn, neighbors[5], costs[0] + costs[2])
+          : null;
+      costs[6] =
+        neighbors[6] && costs[1] && costs[3]
+          ? this.getCost(costFn, neighbors[6], costs[1] + costs[3])
+          : null;
+      costs[7] =
+        neighbors[7] && costs[2] && costs[3]
+          ? this.getCost(costFn, neighbors[7], costs[2] + costs[3])
+          : null;
+
+      neighbors.forEach((neighbor, index) => {
         if (!neighbor) {
           return;
         }
 
-        const cost = costFn(neighbor);
+        const cost = costs[index];
         if (!cost) {
           return;
         }
 
         const neighborHash = neighbor.getHash();
-        const score = gScores.get(currentHash)! + (index > 3 ? 1 : 1.5) * cost; // Diagonals are 1.5 times as expensive
+        const score = gScores.get(currentHash)! + cost;
 
         if (score < (gScores.get(neighborHash) ?? Infinity)) {
           path.set(neighborHash, current);
@@ -126,6 +149,19 @@ class Pathfinder {
       Math.abs(from.getX() - to.getX()) + Math.abs(from.getY() - to.getY())
     );
   }
+
+  private getCost = (
+    costFn: (tile: Tile) => number | null | undefined,
+    tile: Tile,
+    diagonalCosts = 0
+  ) => {
+    const cost = costFn(tile);
+    if (!cost) {
+      return null;
+    }
+
+    return cost + diagonalCosts / 4;
+  };
 }
 
 export default Pathfinder;
