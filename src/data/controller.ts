3f28da7266f89b066bf19fa833b644c807638503
Lennert Claeys <lennert.claeys@teamleader.eu>
2022-04-14 19:32:50 +0200

    Update controller to use placeables

--- src/data/controller.ts
+++ src/data/controller.ts
@@ -1,4 +1,4 @@
-import Tower from "./entity/tower";
+import { isPlaceableTile, Placeable } from "./placeables";
 import Surface from "./terrain/surface";
 import Tile, { TileType } from "./terrain/tile";
 
@@ -6,7 +6,17 @@ class Controller {
   private mouseDownX = 0;
   private mouseDownY = 0;
 
+  private selectedPlacable: Placeable | null = null;
+
   constructor(private surface: Surface) {}
+
+  public getPlacable() {
+    return this.selectedPlacable;
+  }
+
+  public setPlaceable(placeable: Placeable) {
+    this.selectedPlacable = placeable;
+  }
 
   public mouseDown(x: number, y: number) {
     this.mouseDownX = x;
@@ -14,6 +24,10 @@ class Controller {
   }
 
   public mouseUp(x: number, y: number, shiftKey: boolean) {
+    if (!this.selectedPlacable) {
+      return;
+    }
+
     if (shiftKey) {
       if (x > y) {
         y = this.mouseDownY;
@@ -23,7 +37,11 @@ class Controller {
     }
 
     this.surface.forLine(this.mouseDownX, this.mouseDownY, x, y, (tile) => {
-      this.surface.setTile(new Tile(tile.getX(), tile.getY(), TileType.Wall));
+      if (isPlaceableTile(this.selectedPlacable!)) {
+        this.surface.setTile(new Tile(tile.getX(), tile.getY(), TileType.Wall));
+      } else {
+        this.surface.spawnStatic(new this.selectedPlacable!.entity(tile));
+      }
     });
   }
 }
