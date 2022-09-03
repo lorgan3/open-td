30239864bf7159e4cdd950d7a516ee7c852353b2
Lennert Claeys <lennert.claeys@teamleader.eu>
2022-09-03 11:37:23 +0200

    getAdjacentTiles can be removed

--- src/data/util/baseExpansion.ts
+++ src/data/util/baseExpansion.ts
@@ -1,25 +1,10 @@
 import Base from "../entity/base";
 import { StaticAgent } from "../entity/entity";
 import Surface from "../terrain/surface";
-import Tile, { TileType, TileWithStaticEntity } from "../terrain/tile";
-
-const getAdjacentTiles = (tile: Tile, surface: Surface) => {
-  const options: Array<[number, number]> = [
-    [1, 0],
-    [-1, 0],
-    [0, 1],
-    [0, -1],
-  ];
-
-  return options
-    .map(([x, y]) => surface.getTile(tile.getX() + x, tile.getY() + y))
-    .filter(
-      (tile) => !!tile && tile.hasStaticEntity()
-    ) as TileWithStaticEntity[];
-};
+import Tile, { TileType } from "../terrain/tile";
 
 export const canBuild = (tile: Tile, surface: Surface) => {
-  const adjacentTiles = getAdjacentTiles(tile, surface);
+  const adjacentTiles = surface.getAdjacentTiles(tile);
   return !!adjacentTiles.find((tile) => tile.getType() === TileType.Base);
 };
 
@@ -30,20 +15,23 @@ export const canSell = (target: Tile, base: Base, surface: Surface) => {
   const baseTile = base.getTile();
   const filled = new Set<Tile>([baseTile]);
 
-  const tilesToCheck = getAdjacentTiles(baseTile, surface);
+  const tilesToCheck = surface.getAdjacentTiles(baseTile);
   while (tilesToCheck.length > 0) {
     const tile = tilesToCheck.pop()!;
 
+    if (tile === target || filled.has(tile)) {
+      continue;
+    }
+
     if (
-      tile === target ||
-      filled.has(tile) ||
+      !tile.hasStaticEntity() ||
       !base.getParts().has(tile.getStaticEntity().getAgent() as StaticAgent)
     ) {
       continue;
     }
 
     filled.add(tile);
-    getAdjacentTiles(tile, surface).forEach((tile) => tilesToCheck.push(tile));
+    surface.getAdjacentTiles(tile).forEach((tile) => tilesToCheck.push(tile));
   }
 
   return filled.size === base.getParts().size - 1;
