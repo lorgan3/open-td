7c446fd6b9df669de4644a26f1d57ad0ebaa9270
Lennert Claeys <lennert.claeys@teamleader.eu>
2022-09-03 20:12:36 +0200

    Base tiles are also solid

--- src/data/terrain/collision.ts
+++ src/data/terrain/collision.ts
@@ -4,6 +4,7 @@ export const SOLID_TILES = new Set<TileType>([
   TileType.Obstructed,
   TileType.Wall,
   TileType.PlayerBuilding,
+  TileType.Base,
 ]);
 
 export const isSolid = (tile: Tile) => SOLID_TILES.has(tile.getType());
