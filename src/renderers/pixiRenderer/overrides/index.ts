edf855f208580f248269d58950b2eacd52bc33a0
Lennert Claeys <lennert.claeys@teamleader.eu>
2022-11-01 09:49:06 +0100

    Add render override for towers

--- src/renderers/pixiRenderer/overrides/index.ts
+++ src/renderers/pixiRenderer/overrides/index.ts
@@ -3,6 +3,7 @@ import { EntityType } from "../../../data/entity/entity";
 import { Flier } from "./flier";
 import { Regular } from "./regular";
 import { Runner } from "./runner";
+import { Tower } from "./tower";
 
 export interface EntityRenderer extends Sprite {
   sync: () => void;
@@ -14,4 +15,9 @@ export const OVERRIDES: Partial<Record<EntityType, Constructor>> = {
   [EntityType.Runner]: Runner,
   [EntityType.Slime]: Regular,
   [EntityType.Flier]: Flier,
+  [EntityType.Tower]: Tower,
+  [EntityType.Flamethrower]: Tower,
+  [EntityType.Laser]: Tower,
+  [EntityType.Mortar]: Tower,
+  [EntityType.Railgun]: Tower,
 };
---
+++ src/renderers/pixiRenderer/overrides/tower.ts
@@ -0,0 +1,41 @@
+import { AnimatedSprite, Loader } from "pixi.js";
+import { EntityRenderer } from ".";
+
+import { EntityType } from "../../../data/entity/entity";
+import { ITower } from "../../../data/entity/towers";
+import { SCALE } from "../renderer";
+
+const TOWER_TO_ATLAS_MAP = new Map<EntityType, string>([
+  [EntityType.Tower, "turret"],
+  [EntityType.Flamethrower, "flamethrower"],
+  [EntityType.Laser, "laser"],
+  [EntityType.Mortar, "mortar"],
+  [EntityType.Railgun, "railgun"],
+]);
+
+const ANIMATION_SPEED = 0.1;
+
+class Tower extends AnimatedSprite implements EntityRenderer {
+  constructor(private data: ITower, loader: Loader) {
+    const atlasName = TOWER_TO_ATLAS_MAP.get(data.getType())!;
+    super(Object.values(loader.resources[atlasName].spritesheet!.textures));
+    this.pivot = { x: 32, y: 32 };
+    this.animationSpeed = ANIMATION_SPEED;
+    this.loop = false;
+  }
+
+  sync() {
+    this.position.set(
+      this.data.entity.getX() * SCALE + this.pivot.x,
+      this.data.entity.getY() * SCALE + this.pivot.y
+    );
+    this.angle = this.data.entity.getRotation();
+
+    if (this.data.renderData.fired) {
+      this.data.renderData.fired = false;
+      this.play();
+    }
+  }
+}
+
+export { Tower };
