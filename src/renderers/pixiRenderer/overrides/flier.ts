2ff14ff60cdb7d47dd72578ca34e679bfaba0a8c
Lennert Claeys <lennert.claeys@teamleader.eu>
2022-11-11 17:30:44 +0100

    Fix enemy alignment and they can catch on fire now

--- src/renderers/pixiRenderer/overrides/flier.ts
+++ src/renderers/pixiRenderer/overrides/flier.ts
@@ -1,15 +1,19 @@
-import { AnimatedSprite, Loader } from "pixi.js";
+import { AnimatedSprite, Loader, Sprite } from "pixi.js";
 import { EntityRenderer } from ".";
+import { Status } from "../../../data/entity/enemies";
 import FlierData from "../../../data/entity/enemies/flier";
 import { SCALE } from "../renderer";
+import { FIRE_ATLAS_NAME, FIRE_SPRITE } from "./flame";
 
 const ATLAS_NAME = "flier";
 const ANIMATION_SPEED = 0.2;
 
 class Flier extends AnimatedSprite implements EntityRenderer {
-  constructor(private data: FlierData, loader: Loader) {
+  private flames: Sprite[] = [];
+
+  constructor(private data: FlierData, private loader: Loader) {
     super(Object.values(loader.resources[ATLAS_NAME].spritesheet!.textures));
-    this.pivot = { x: 16, y: 16 };
+    this.anchor.set(0.5);
     this.animationSpeed = ANIMATION_SPEED;
 
     this.play();
@@ -17,10 +21,43 @@ class Flier extends AnimatedSprite implements EntityRenderer {
 
   sync() {
     this.position.set(
-      this.data.entity.getX() * SCALE,
-      this.data.entity.getY() * SCALE
+      (this.data.entity.getX() + 0.5) * SCALE,
+      (this.data.entity.getY() + 0.5) * SCALE
     );
     this.angle = this.data.entity.getRotation();
+
+    const offset = SCALE / 2;
+    this.flames.forEach((flame) => {
+      flame.angle = -this.data.entity.getRotation();
+      flame.position.set(
+        Math.random() * offset - offset / 2,
+        Math.random() * offset - offset / 2
+      );
+    });
+
+    if (this.data.getStatus() === Status.OnFire && !this.flames.length) {
+      this.tint = 0xe25822;
+
+      for (let i = 0; i < 2; i++) {
+        const flame = new Sprite(
+          this.loader.resources[FIRE_ATLAS_NAME].spritesheet!.textures[
+            FIRE_SPRITE
+          ]
+        );
+        flame.alpha = 0.8;
+        flame.anchor.set(0.5);
+        flame.position.set(
+          Math.random() * offset - offset / 2,
+          Math.random() * offset - offset / 2
+        );
+        this.addChild(flame);
+        this.flames.push(flame);
+      }
+    } else if (this.data.getStatus() !== Status.OnFire && this.flames.length) {
+      this.tint = 0xffffff;
+      this.removeChildren();
+      this.flames = [];
+    }
   }
 }
 
--- src/renderers/pixiRenderer/overrides/runner.ts
+++ src/renderers/pixiRenderer/overrides/runner.ts
@@ -1,27 +1,64 @@
-import { AnimatedSprite, Loader } from "pixi.js";
+import { AnimatedSprite, Loader, Sprite } from "pixi.js";
 import { EntityRenderer } from ".";
+import { Status } from "../../../data/entity/enemies";
 import RunnerData from "../../../data/entity/enemies/runner";
 import { SCALE } from "../renderer";
+import { FIRE_ATLAS_NAME, FIRE_SPRITE } from "./flame";
 
 const ATLAS_NAME = "runner";
 const ANIMATION_SPEED = 0.1;
 
 class Runner extends AnimatedSprite implements EntityRenderer {
-  constructor(private data: RunnerData, loader: Loader) {
+  private flames: Sprite[] = [];
+
+  constructor(private data: RunnerData, private loader: Loader) {
     super(Object.values(loader.resources[ATLAS_NAME].spritesheet!.textures));
-    this.pivot = { x: 16, y: 16 };
+    this.anchor.set(0.5);
     this.animationSpeed = ANIMATION_SPEED;
   }
 
   sync() {
     this.position.set(
-      this.data.entity.getX() * SCALE,
-      this.data.entity.getY() * SCALE
+      (this.data.entity.getX() + 0.5) * SCALE,
+      (this.data.entity.getY() + 0.5) * SCALE
     );
     this.angle = this.data.entity.getRotation();
 
     if (this.data.AI.isBusy() === this.playing) {
       this.playing ? this.stop() : this.play();
+    }
+
+    const offset = SCALE / 2;
+    this.flames.forEach((flame) => {
+      flame.angle = -this.data.entity.getRotation();
+      flame.position.set(
+        Math.random() * offset - offset / 2,
+        Math.random() * offset - offset / 2
+      );
+    });
+
+    if (this.data.getStatus() === Status.OnFire && !this.flames.length) {
+      this.tint = 0xe25822;
+
+      for (let i = 0; i < 2; i++) {
+        const flame = new Sprite(
+          this.loader.resources[FIRE_ATLAS_NAME].spritesheet!.textures[
+            FIRE_SPRITE
+          ]
+        );
+        flame.alpha = 0.8;
+        flame.anchor.set(0.5);
+        flame.position.set(
+          Math.random() * offset - offset / 2,
+          Math.random() * offset - offset / 2
+        );
+        this.addChild(flame);
+        this.flames.push(flame);
+      }
+    } else if (this.data.getStatus() !== Status.OnFire && this.flames.length) {
+      this.tint = 0xffffff;
+      this.removeChildren();
+      this.flames = [];
     }
   }
 }
