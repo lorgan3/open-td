518185adb5305c889803e9da82a9cf03d9c15227
Lennert Claeys <lennert.claeys@teamleader.eu>
2022-11-22 20:00:19 +0100

    Add utilities for working with objects

---
+++ src/util/__spec__/object.spec.ts
@@ -0,0 +1,23 @@
+import { assertValue, getKey } from "../object";
+
+describe("object", () => {
+  const object = { foo: 1, bar: 2, baz: 3 };
+
+  describe("getKey", () => {
+    it("finds the value in an object", () => {
+      expect(getKey(object, 2)).toEqual("bar");
+
+      expect(getKey(object, 5)).toBeUndefined();
+      expect(getKey(object, 5, "foo")).toEqual("foo");
+    });
+  });
+
+  describe("assertValue", () => {
+    it("returns the value if it's part of the object", () => {
+      expect(assertValue(object, 2)).toEqual(2);
+
+      expect(assertValue(object, 5)).toBeUndefined();
+      expect(assertValue(object, 5, 1)).toEqual(1);
+    });
+  });
+});
