05b3ad57a18d4ea5ec6b2e9eb75b8b5fae5cc133
Lennert Claeys <claeyslennert@gmail.com>
2023-03-04 22:04:31 +0100

    Update baseGenerator to handle corners

--- scripts/baseGenerator.ts
+++ scripts/baseGenerator.ts
@@ -4,7 +4,7 @@ import { NamedImage, write } from "./util.js";
 const SCALE = 64;
 const BORDER_WIDTH = 4;
 
-const atlas = await Jimp.read("./scripts/base.png");
+const atlas = await Jimp.read("./scripts/wallTest2.png");
 const background = new Jimp(SCALE, SCALE).blit(atlas, 0, 0, 0, 0, SCALE, SCALE);
 
 const borders = new Jimp(SCALE, SCALE).blit(
@@ -24,24 +24,76 @@ images.push({ image: background, name: "base-0000" });
 // Left
 images.push({
   image: new Jimp(SCALE, SCALE)
-    .blit(background, 1, 0, 1, 0, SCALE - 1, SCALE)
-    .blit(borders, 0, 0, 0, 0, BORDER_WIDTH, SCALE),
+    .blit(
+      background,
+      BORDER_WIDTH,
+      0,
+      BORDER_WIDTH,
+      0,
+      SCALE - BORDER_WIDTH,
+      SCALE
+    )
+    .blit(borders, 0, 0, 0, 0, BORDER_WIDTH, SCALE)
+    .blit(borders, 0, 0, 0, SCALE / 2, BORDER_WIDTH, BORDER_WIDTH)
+    .blit(
+      borders,
+      0,
+      SCALE - BORDER_WIDTH,
+      0,
+      SCALE / 2,
+      BORDER_WIDTH,
+      BORDER_WIDTH
+    ),
   name: "base-0001",
 });
 
 // Left + top
 images.push({
   image: new Jimp(SCALE, SCALE)
-    .blit(background, 1, 1, 1, 1, SCALE - 1, SCALE - 1)
+    .blit(
+      background,
+      BORDER_WIDTH,
+      BORDER_WIDTH,
+      BORDER_WIDTH,
+      BORDER_WIDTH,
+      SCALE - BORDER_WIDTH,
+      SCALE - BORDER_WIDTH
+    )
     .blit(borders, 0, 0, 0, 0, BORDER_WIDTH, SCALE)
-    .blit(borders, 0, 0, 0, 0, SCALE, BORDER_WIDTH),
+    .blit(borders, 0, 0, 0, 0, SCALE, BORDER_WIDTH)
+    .blit(
+      borders,
+      0,
+      SCALE - BORDER_WIDTH,
+      0,
+      SCALE / 2,
+      BORDER_WIDTH,
+      BORDER_WIDTH
+    )
+    .blit(
+      borders,
+      SCALE - BORDER_WIDTH,
+      0,
+      SCALE / 2,
+      0,
+      BORDER_WIDTH,
+      BORDER_WIDTH
+    ),
   name: "base-1001",
 });
 
 // Left + top + right
 images.push({
   image: new Jimp(SCALE, SCALE)
-    .blit(background, 1, 1, 1, 1, SCALE - 2, SCALE - 1)
+    .blit(
+      background,
+      BORDER_WIDTH,
+      BORDER_WIDTH,
+      BORDER_WIDTH,
+      BORDER_WIDTH,
+      SCALE - BORDER_WIDTH * 2,
+      SCALE - BORDER_WIDTH
+    )
     .blit(borders, 0, 0, 0, 0, BORDER_WIDTH, SCALE)
     .blit(borders, 0, 0, 0, 0, SCALE, BORDER_WIDTH)
     .blit(
@@ -52,6 +104,24 @@ images.push({
       0,
       BORDER_WIDTH,
       SCALE
+    )
+    .blit(
+      borders,
+      0,
+      SCALE - BORDER_WIDTH,
+      0,
+      SCALE / 2,
+      BORDER_WIDTH,
+      BORDER_WIDTH
+    )
+    .blit(
+      borders,
+      SCALE - BORDER_WIDTH,
+      SCALE - BORDER_WIDTH,
+      SCALE - BORDER_WIDTH,
+      SCALE / 2,
+      BORDER_WIDTH,
+      BORDER_WIDTH
     ),
   name: "base-1101",
 });
@@ -59,15 +129,41 @@ images.push({
 // Top
 images.push({
   image: new Jimp(SCALE, SCALE)
-    .blit(background, 0, 1, 0, 1, SCALE, SCALE - 1)
-    .blit(borders, 0, 0, 0, 0, SCALE, BORDER_WIDTH),
+    .blit(
+      background,
+      0,
+      BORDER_WIDTH,
+      0,
+      BORDER_WIDTH,
+      SCALE,
+      SCALE - BORDER_WIDTH
+    )
+    .blit(borders, 0, 0, 0, 0, SCALE, BORDER_WIDTH)
+    .blit(borders, 0, 0, SCALE / 2, 0, BORDER_WIDTH, BORDER_WIDTH)
+    .blit(
+      borders,
+      SCALE - BORDER_WIDTH,
+      0,
+      SCALE / 2,
+      0,
+      BORDER_WIDTH,
+      BORDER_WIDTH
+    ),
   name: "base-1000",
 });
 
 // Top + right
 images.push({
   image: new Jimp(SCALE, SCALE)
-    .blit(background, 0, 1, 0, 1, SCALE - 1, SCALE - 1)
+    .blit(
+      background,
+      0,
+      BORDER_WIDTH,
+      0,
+      BORDER_WIDTH,
+      SCALE - BORDER_WIDTH,
+      SCALE - BORDER_WIDTH
+    )
     .blit(borders, 0, 0, 0, 0, SCALE, BORDER_WIDTH)
     .blit(
       borders,
@@ -77,6 +173,16 @@ images.push({
       0,
       BORDER_WIDTH,
       SCALE
+    )
+    .blit(borders, 0, 0, SCALE / 2, 0, BORDER_WIDTH, BORDER_WIDTH)
+    .blit(
+      borders,
+      SCALE - BORDER_WIDTH,
+      SCALE - BORDER_WIDTH,
+      SCALE - BORDER_WIDTH,
+      SCALE / 2,
+      BORDER_WIDTH,
+      BORDER_WIDTH
     ),
   name: "base-1100",
 });
@@ -84,7 +190,15 @@ images.push({
 // Top + right + bottom
 images.push({
   image: new Jimp(SCALE, SCALE)
-    .blit(background, 0, 1, 0, 1, SCALE - 1, SCALE - 2)
+    .blit(
+      background,
+      0,
+      BORDER_WIDTH,
+      0,
+      BORDER_WIDTH,
+      SCALE - BORDER_WIDTH,
+      SCALE - BORDER_WIDTH * 2
+    )
     .blit(borders, 0, 0, 0, 0, SCALE, BORDER_WIDTH)
     .blit(
       borders,
@@ -102,6 +216,16 @@ images.push({
       0,
       SCALE - BORDER_WIDTH,
       SCALE,
+      BORDER_WIDTH
+    )
+    .blit(borders, 0, 0, SCALE / 2, 0, BORDER_WIDTH, BORDER_WIDTH)
+    .blit(
+      borders,
+      0,
+      SCALE - BORDER_WIDTH,
+      SCALE / 2,
+      SCALE - BORDER_WIDTH,
+      BORDER_WIDTH,
       BORDER_WIDTH
     ),
   name: "base-1110",
@@ -110,7 +234,7 @@ images.push({
 // Right
 images.push({
   image: new Jimp(SCALE, SCALE)
-    .blit(background, 0, 0, 0, 0, SCALE - 1, SCALE)
+    .blit(background, 0, 0, 0, 0, SCALE - BORDER_WIDTH, SCALE)
     .blit(
       borders,
       SCALE - BORDER_WIDTH,
@@ -119,6 +243,24 @@ images.push({
       0,
       BORDER_WIDTH,
       SCALE
+    )
+    .blit(
+      borders,
+      SCALE - BORDER_WIDTH,
+      0,
+      SCALE - BORDER_WIDTH,
+      SCALE / 2,
+      BORDER_WIDTH,
+      BORDER_WIDTH
+    )
+    .blit(
+      borders,
+      SCALE - BORDER_WIDTH,
+      SCALE - BORDER_WIDTH,
+      SCALE - BORDER_WIDTH,
+      SCALE / 2,
+      BORDER_WIDTH,
+      BORDER_WIDTH
     ),
   name: "base-0100",
 });
@@ -126,7 +268,7 @@ images.push({
 // Right + bottom
 images.push({
   image: new Jimp(SCALE, SCALE)
-    .blit(background, 0, 0, 0, 0, SCALE - 1, SCALE - 1)
+    .blit(background, 0, 0, 0, 0, SCALE - BORDER_WIDTH, SCALE - BORDER_WIDTH)
     .blit(
       borders,
       SCALE - BORDER_WIDTH,
@@ -143,6 +285,24 @@ images.push({
       0,
       SCALE - BORDER_WIDTH,
       SCALE,
+      BORDER_WIDTH
+    )
+    .blit(
+      borders,
+      SCALE - BORDER_WIDTH,
+      0,
+      SCALE - BORDER_WIDTH,
+      SCALE / 2,
+      BORDER_WIDTH,
+      BORDER_WIDTH
+    )
+    .blit(
+      borders,
+      0,
+      SCALE - BORDER_WIDTH,
+      SCALE / 2,
+      SCALE - BORDER_WIDTH,
+      BORDER_WIDTH,
       BORDER_WIDTH
     ),
   name: "base-0110",
@@ -151,7 +311,15 @@ images.push({
 // Right + bottom + left
 images.push({
   image: new Jimp(SCALE, SCALE)
-    .blit(background, 1, 0, 1, 0, SCALE - 2, SCALE - 1)
+    .blit(
+      background,
+      BORDER_WIDTH,
+      0,
+      BORDER_WIDTH,
+      0,
+      SCALE - BORDER_WIDTH * 2,
+      SCALE - BORDER_WIDTH
+    )
     .blit(
       borders,
       SCALE - BORDER_WIDTH,
@@ -161,56 +329,6 @@ images.push({
       BORDER_WIDTH,
       SCALE
     )
-    .blit(
-      borders,
-      0,
-      SCALE - BORDER_WIDTH,
-      0,
-      SCALE - BORDER_WIDTH,
-      SCALE,
-      BORDER_WIDTH
-    )
-    .blit(borders, 0, 0, 0, 0, BORDER_WIDTH, SCALE),
-  name: "base-0111",
-});
-
-// Bottom
-images.push({
-  image: new Jimp(SCALE, SCALE)
-    .blit(background, 0, 0, 0, 0, SCALE, SCALE - 1)
-    .blit(
-      borders,
-      0,
-      SCALE - BORDER_WIDTH,
-      0,
-      SCALE - BORDER_WIDTH,
-      SCALE,
-      BORDER_WIDTH
-    ),
-  name: "base-0010",
-});
-
-// Bottom + left
-images.push({
-  image: new Jimp(SCALE, SCALE)
-    .blit(background, 1, 0, 1, 0, SCALE - 1, SCALE - 1)
-    .blit(
-      borders,
-      0,
-      SCALE - BORDER_WIDTH,
-      0,
-      SCALE - BORDER_WIDTH,
-      SCALE,
-      BORDER_WIDTH
-    )
-    .blit(borders, 0, 0, 0, 0, BORDER_WIDTH, SCALE),
-  name: "base-0011",
-});
-
-// Bottom + left + top
-images.push({
-  image: new Jimp(SCALE, SCALE)
-    .blit(background, 1, 1, 1, 1, SCALE - 1, SCALE - 2)
     .blit(
       borders,
       0,
@@ -221,14 +339,144 @@ images.push({
       BORDER_WIDTH
     )
     .blit(borders, 0, 0, 0, 0, BORDER_WIDTH, SCALE)
-    .blit(borders, 0, 0, 0, 0, SCALE, BORDER_WIDTH),
+    .blit(
+      borders,
+      SCALE - BORDER_WIDTH,
+      0,
+      SCALE - BORDER_WIDTH,
+      SCALE / 2,
+      BORDER_WIDTH,
+      BORDER_WIDTH
+    )
+    .blit(borders, 0, 0, 0, SCALE / 2, BORDER_WIDTH, BORDER_WIDTH),
+  name: "base-0111",
+});
+
+// Bottom
+images.push({
+  image: new Jimp(SCALE, SCALE)
+    .blit(background, 0, 0, 0, 0, SCALE, SCALE - BORDER_WIDTH)
+    .blit(
+      borders,
+      0,
+      SCALE - BORDER_WIDTH,
+      0,
+      SCALE - BORDER_WIDTH,
+      SCALE,
+      BORDER_WIDTH
+    )
+    .blit(
+      borders,
+      0,
+      SCALE - BORDER_WIDTH,
+      SCALE / 2,
+      SCALE - BORDER_WIDTH,
+      BORDER_WIDTH,
+      BORDER_WIDTH
+    )
+    .blit(
+      borders,
+      SCALE - BORDER_WIDTH,
+      SCALE - BORDER_WIDTH,
+      SCALE / 2,
+      SCALE - BORDER_WIDTH,
+      BORDER_WIDTH,
+      BORDER_WIDTH
+    ),
+  name: "base-0010",
+});
+
+// Bottom + left
+images.push({
+  image: new Jimp(SCALE, SCALE)
+    .blit(
+      background,
+      BORDER_WIDTH,
+      0,
+      BORDER_WIDTH,
+      0,
+      SCALE - BORDER_WIDTH,
+      SCALE - BORDER_WIDTH
+    )
+    .blit(
+      borders,
+      0,
+      SCALE - BORDER_WIDTH,
+      0,
+      SCALE - BORDER_WIDTH,
+      SCALE,
+      BORDER_WIDTH
+    )
+    .blit(borders, 0, 0, 0, 0, BORDER_WIDTH, SCALE)
+    .blit(
+      borders,
+      SCALE - BORDER_WIDTH,
+      SCALE - BORDER_WIDTH,
+      SCALE / 2,
+      SCALE - BORDER_WIDTH,
+      BORDER_WIDTH,
+      BORDER_WIDTH
+    )
+    .blit(borders, 0, 0, 0, SCALE / 2, BORDER_WIDTH, BORDER_WIDTH),
+  name: "base-0011",
+});
+
+// Bottom + left + top
+images.push({
+  image: new Jimp(SCALE, SCALE)
+    .blit(
+      background,
+      BORDER_WIDTH,
+      BORDER_WIDTH,
+      BORDER_WIDTH,
+      BORDER_WIDTH,
+      SCALE - BORDER_WIDTH,
+      SCALE - BORDER_WIDTH * 2
+    )
+    .blit(
+      borders,
+      0,
+      SCALE - BORDER_WIDTH,
+      0,
+      SCALE - BORDER_WIDTH,
+      SCALE,
+      BORDER_WIDTH
+    )
+    .blit(borders, 0, 0, 0, 0, BORDER_WIDTH, SCALE)
+    .blit(borders, 0, 0, 0, 0, SCALE, BORDER_WIDTH)
+    .blit(
+      borders,
+      SCALE - BORDER_WIDTH,
+      SCALE - BORDER_WIDTH,
+      SCALE / 2,
+      SCALE - BORDER_WIDTH,
+      BORDER_WIDTH,
+      BORDER_WIDTH
+    )
+    .blit(
+      borders,
+      SCALE - BORDER_WIDTH,
+      0,
+      SCALE / 2,
+      0,
+      BORDER_WIDTH,
+      BORDER_WIDTH
+    ),
   name: "base-1011",
 });
 
 // Bottom + left + top + right
 images.push({
   image: new Jimp(SCALE, SCALE)
-    .blit(background, 1, 1, 1, 1, SCALE - 2, SCALE - 2)
+    .blit(
+      background,
+      BORDER_WIDTH,
+      BORDER_WIDTH,
+      BORDER_WIDTH,
+      BORDER_WIDTH,
+      SCALE - BORDER_WIDTH * 2,
+      SCALE - BORDER_WIDTH * 2
+    )
     .blit(
       borders,
       0,
@@ -255,7 +503,15 @@ images.push({
 // Left + right
 images.push({
   image: new Jimp(SCALE, SCALE)
-    .blit(background, 1, 0, 1, 0, SCALE - 2, SCALE)
+    .blit(
+      background,
+      BORDER_WIDTH,
+      0,
+      BORDER_WIDTH,
+      0,
+      SCALE - BORDER_WIDTH * 2,
+      SCALE
+    )
     .blit(borders, 0, 0, 0, 0, BORDER_WIDTH, SCALE)
     .blit(
       borders,
@@ -265,6 +521,34 @@ images.push({
       0,
       BORDER_WIDTH,
       SCALE
+    )
+    .blit(borders, 0, 0, 0, SCALE / 2, BORDER_WIDTH, BORDER_WIDTH)
+    .blit(
+      borders,
+      0,
+      SCALE - BORDER_WIDTH,
+      0,
+      SCALE / 2,
+      BORDER_WIDTH,
+      BORDER_WIDTH
+    )
+    .blit(
+      borders,
+      SCALE - BORDER_WIDTH,
+      0,
+      SCALE - BORDER_WIDTH,
+      SCALE / 2,
+      BORDER_WIDTH,
+      BORDER_WIDTH
+    )
+    .blit(
+      borders,
+      SCALE - BORDER_WIDTH,
+      SCALE - BORDER_WIDTH,
+      SCALE - BORDER_WIDTH,
+      SCALE / 2,
+      BORDER_WIDTH,
+      BORDER_WIDTH
     ),
   name: "base-0101",
 });
@@ -272,7 +556,15 @@ images.push({
 // Top + bottom
 images.push({
   image: new Jimp(SCALE, SCALE)
-    .blit(background, 0, 1, 0, 1, SCALE, SCALE - 2)
+    .blit(
+      background,
+      0,
+      BORDER_WIDTH,
+      0,
+      BORDER_WIDTH,
+      SCALE,
+      SCALE - BORDER_WIDTH * 2
+    )
     .blit(borders, 0, 0, 0, 0, SCALE, BORDER_WIDTH)
     .blit(
       borders,
@@ -281,6 +573,34 @@ images.push({
       0,
       SCALE - BORDER_WIDTH,
       SCALE,
+      BORDER_WIDTH
+    )
+    .blit(borders, 0, 0, SCALE / 2, 0, BORDER_WIDTH, BORDER_WIDTH)
+    .blit(
+      borders,
+      SCALE - BORDER_WIDTH,
+      0,
+      SCALE / 2,
+      0,
+      BORDER_WIDTH,
+      BORDER_WIDTH
+    )
+    .blit(
+      borders,
+      0,
+      SCALE - BORDER_WIDTH,
+      SCALE / 2,
+      SCALE - BORDER_WIDTH,
+      BORDER_WIDTH,
+      BORDER_WIDTH
+    )
+    .blit(
+      borders,
+      SCALE - BORDER_WIDTH,
+      SCALE - BORDER_WIDTH,
+      SCALE / 2,
+      SCALE - BORDER_WIDTH,
+      BORDER_WIDTH,
       BORDER_WIDTH
     ),
   name: "base-1010",
